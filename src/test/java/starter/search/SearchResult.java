d897eeded22cfd44c412d229035a73256a7bb795
Julian <jdmesalosada@gmail.com>
2022-10-27 11:27:56 -0500

    removed unused code

--- src/test/java/starter/search/SearchResult.java
+++ src/test/java/starter/search/SearchResult.java
@@ -2,14 +2,11 @@ package starter.search;
 
 import net.serenitybdd.screenplay.Question;
 import net.serenitybdd.screenplay.questions.Text;
-import net.serenitybdd.screenplay.questions.TextContent;
 
 import java.util.Collection;
-import java.util.List;
 
 public class SearchResult {
     public static Question<Collection<String>> titles() {
-        //return actor -> TextContent.of(SearchResultList.RESULT_TITLES).viewedBy(actor).asList();
         return Text.ofEach(SearchResultList.RESULT_TITLES).describedAs("the results displayed");
     }
 }
