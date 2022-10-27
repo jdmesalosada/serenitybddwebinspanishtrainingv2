ecee75cafd193bd213bbdfba168f20bca148116d
Julian <jdmesalosada@gmail.com>
2022-10-27 11:41:10 -0500

    removed drivers sections. Adjusted locators

--- src/test/java/starter/search/SearchForm.java
+++ src/test/java/starter/search/SearchForm.java
@@ -3,6 +3,6 @@ package starter.search;
 import org.openqa.selenium.By;
 
 public class SearchForm {
-    public static By SEARCH_FIELD = By.id("search_form_input_homepage");
-    public static By SEARCH_BUTTON = By.id("search_button_homepage");
+    public static By SEARCH_FIELD = By.id("searchbox_input");
+    public static By SEARCH_BUTTON = By.cssSelector("button[class*='searchbox_searchButton']");
 }
